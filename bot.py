613482229ba13ca8f9d67cc0e203aed451e59a85
MO4OO3O3 <45795994+MO4OO3O3@users.noreply.github.com>
2018-12-11 16:10:30 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ async def on_ready():
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
 
-bot.run('<YOUR_TOKEN_HERE>')
+bot.run('<NTIyMDc4NzU4NDUxMjgxOTIx.DvFy2A.yuqHZvNIVVJ6h-jRRA-wJ6QOXLg')
