e50afea7ad01cd765fcc8fd3c2167463e2de8294
MO4OO3O3 <45795994+MO4OO3O3@users.noreply.github.com>
2018-12-12 12:46:52 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,12 @@ async def ip(ctx):
 async def afonso(ctx):
     await ctx.send("`I'm afonso and i'm gay`")
 
-    
+@bot.command()
+async def projetos(ctx):
+    await ctx.send("Meus Projetos:")
+    await ctx.send("https://gamersboard.com.br/topic/63623-morais-minas-ganhe-dinheiro-ao-minerar/")
+    await ctx.send("")
+    await ctx.send("No futuro irei adicionar mais!")
     
     
 bot.run('NTIyMDc4NzU4NDUxMjgxOTIx.DvFy2A.yuqHZvNIVVJ6h-jRRA-wJ6QOXLg')
